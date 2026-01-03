be1695dfeee4c3b27692a0e23ba0d773c57e8465
lewisc99 <lewiscontato99@gmail.com>
2026-01-03 20:30:35 -0300

    Add two singleton pattern implementations for MyDatabase

--- Creational/Singleton/SingletonByConvention.cs
+++ Creational/Singleton/SingletonByConvention.cs
@@ -69,6 +69,10 @@ namespace HidingConstructor
 
 namespace LazyLoading
 {
+    /// <summary>
+    /// The goal of this code is to create a "Database" object that is created only once. No matter how many times you ask for the database in your application, you will always get the exact same object. This is often done to manage shared resources like database connections.
+    /// </summary>
+    /// 
     public class MyDatabase
     {
         private MyDatabase()
@@ -78,6 +82,48 @@ namespace LazyLoading
 
         private static Lazy<MyDatabase> instance = new(() => new MyDatabase());
         public static MyDatabase Instance => instance.Value;
+    }
+
+    public class LazyLoadingResult
+    {
+        public void Result()
+        {
+            // The database is NOT created yet.
+
+            var db1 = MyDatabase.Instance; // Prints: "Initializing database"
+            var db2 = MyDatabase.Instance; // Does NOT print anything. Returns the same object.
+
+            bool areSame = (db1 == db2);   // True. They are the same instance.
+        }
+    }
+}
+
+namespace LazyReusableBaseClass
+{
+
+    /// <summary>
+    /// public class MyDatabase : Singleton<MyDatabase>: This line says, "I am MyDatabase, and I want to use the Singleton blueprint. Use Me (MyDatabase) as the placeholder T."
+
+    /// By inheriting, MyDatabase should automatically get the Instance property from the parent.
+    /// </summary>
+    public class MyDatabase : Singleton<MyDatabase>
+    {
+        private MyDatabase()
+        {
+            Console.WriteLine("Initializing database");
+        }
+
+        //private static Lazy<MyDatabase> instance = new(() => new MyDatabase());
+        //public static MyDatabase Instance => instance.Value;
+    }
+
+
+    public abstract class Singleton<T> where T : Singleton<T>
+    {
+        private static readonly Lazy<T> lazy =
+            new Lazy<T>(() => Activator.CreateInstance(typeof(T), true) as T);
+
+        public static T Instance => lazy.Value;
     }
 
     public class LazyLoadingResult
