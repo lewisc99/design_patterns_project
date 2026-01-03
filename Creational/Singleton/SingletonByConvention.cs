f18555fbbb07048f0a238b2d8bfa91f43a0d85d0
lewisc99 <lewiscontato99@gmail.com>
2026-01-02 21:27:41 -0300

    Add three singleton pattern implementations in C#

---
+++ Creational/Singleton/SingletonByConvention.cs
@@ -0,0 +1,71 @@
+﻿using SingletonByConvention;
+using System;
+
+namespace SingletonByConvention
+{
+    public class Database
+    {
+
+        //Please do not create more than on instance
+        public Database() { }
+    }
+
+    public static class Globals
+    {
+        public static Database Database = new Database();
+    }
+
+    public class SingletonByConvention
+    {
+        public void Result()
+        {
+            Database database = Globals.Database;
+        }
+    }
+}
+
+namespace ClassicImplementation
+{
+    public class Database
+    {
+        private static int instanceCount = 0;
+
+        public Database()
+        {
+            if (++instanceCount > 1)
+                throw new InvalidOperationException("Cannot make >1 database!");
+        }
+    }
+
+    public class ClassicImplementationResult
+    {
+        public void Result()
+        {
+            Database database = new Database();
+
+
+            // throws an exception
+            Database database1 = new Database();
+        }
+    }
+}
+
+namespace HidingConstructor
+{
+    public class Database
+    {
+        private Database() { }
+        public static Database Instance { get; } = new();
+    }
+
+    public class HidingConstructorResult
+    {
+        public void Result()
+        {
+            Database database = Database.Instance;
+
+            // show an error
+            //Database database1 = new Database();
+        }
+    }
+}
